3e6c5024d6920e58912d9f76de022c7cd5cc77f5
Ahu <dxqllp@163.com>
2024-03-18 10:11:21 +0800

    Delete WSSL directory

--- WSSL/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-from .detr import build
-from .position_encoding import build_position_encoding
-
-
-
-
-def build_model(args):
-    return build(args)
